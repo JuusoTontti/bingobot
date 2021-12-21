331c08542dc4327fd42c69b8d89adef76e49a683
JuusoTontti <47010138+JuusoTontti@users.noreply.github.com>
2021-12-22 01:01:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -32,17 +32,13 @@ class Bot(commands.Bot):
             self.osallistujat = []
             self.tulokset = []
             self.tulos = None
-            await ctx.send(f"@{ctx.author.name} -> Bingo aloitettu")
-        else:
-           await ctx.send("@" + ctx.author.name + " -> Sinulla ei ole oikeuksia tähän komentoon.")
-    
+            await ctx.send(f'@{ctx.author.name} -> Bingo aloitettu')
+
     @commands.command()
     async def sulje(self,ctx: commands.Context):
         if ctx.author.is_mod or ctx.author.name == os.environ['CHANNEL']:
             self.isOpen = False
             await ctx.send(f'@{ctx.author.name} osallistuminen suljettu')
-        else:
-            await ctx.send("@" + ctx.author.name + " -> Sinulla ei ole oikeuksia tähän komentoon.")
 
     @commands.command()
     async def bingo(self,ctx: commands.Context):
@@ -53,24 +49,23 @@ class Bot(commands.Bot):
                 result = re.sub(regex, subst, ctx.message.content)
                 tulosregex = r"^((30)|([0-2]?[0-9]{1,1}))$"
                 if re.match(tulosregex,result):
-                    await ctx.send(f'{ctx.author.name} vastauksesi on rekisteröity!')
+                    await ctx.send(f'@{ctx.author.name} vastauksesi on rekisteröity!')
                     self.osallistujat.append(ctx.author.name)
                     self.tulokset.append(result)
                 else:
                     await ctx.send(f'@{ctx.author.name} tuloksesi ei ole 0-30 alueen sisällä!')
             else:
-                await ctx.send(f'@{ctx.author.name} -> Bingo osallistuminen on tällä hetkellä suljettu')
+                await ctx.send(f'@{ctx.author.name} -> Bingoon osallistuminen on tällä hetkellä suljettu')
 
     @commands.command()
     async def tulos(self,ctx: commands.Context):
-        if re.search("!tulos", ctx.message.content):
-             regex = r"!tulos "
-             subst = ""
-             result = re.sub(regex, subst, ctx.message.content)
-             await ctx.send(f'{ctx.author.name} tulos asetettu nyt voit listaa voittajat käyttämällä !listaa')
-             self.tulos = result
-        else:
-                await ctx.send(f'@{ctx.author.name} -> Sinulla ei ole oikeuksia tähän komentoon.')
+        if ctx.author.is_mod or ctx.author.name == os.environ['CHANNEL']:
+            if re.search("!tulos", ctx.message.content):
+                regex = r"!tulos "
+                subst = ""
+                result = re.sub(regex, subst, ctx.message.content)
+                await ctx.send(f'@{ctx.author.name} tulos asetettu nyt voit listaa voittajat käyttämällä !listaa')
+                self.tulos = result
 
     @commands.command()
     async def listaa(self,ctx: commands.Context):
@@ -80,13 +75,11 @@ class Bot(commands.Bot):
             if tulos in dictionary.values():
                 for self.osallistujat, self.tulokset in dictionary.items():
                     if self.tulos == None:
-                        await ctx.send(f'{ctx.author.name} -> Kierroksen tulosta ei ole vielä asetettu, moderaattorit asettakaa tulos käyttämällä !tulos komentoa.')
+                        await ctx.send(f'@{ctx.author.name} -> Kierroksen tulosta ei ole vielä asetettu, moderaattorit asettakaa tulos käyttämällä !tulos komentoa.')
                     if self.tulokset == tulos:
                         await ctx.send(f'@{ctx.author.name} -> voittajia ovat {self.osallistujat}')
             else:
                 await ctx.send(f'@{ctx.author.name} Ei voittajia!')
-        else:
-            await ctx.send("@" + ctx.author.name + " -> Sinulla ei ole oikeuksia tähän komentoon.")
 
 bot = Bot()
 bot.run()
